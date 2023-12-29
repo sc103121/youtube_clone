322c4936a6a3ff26bba8558d0172ee77429a76d9
sc103121 <shawnchencsy@gmail.com>
2023-12-28 21:55:48 -0500

    Push full-stack app

---
+++ yt-web-client/app/navbar/navbar.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./navbar.module.css";
+import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
+import Upload from "./upload";
+
+// closure
+
+export default function Navbar() {
+	// Init user state
+	const [user, setUser] = useState<User | null>(null);
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChangedHelper((user) => {
+			setUser(user);
+		});
+
+		return () => unsubscribe();
+	});
+
+	return (
+		<nav className={styles.nav}>
+			<Link href="/">
+				<Image width={90} height={20}
+					src="/youtube-logo.svg" alt="YouTube Logo"/>
+			</Link>
+			{
+				user && <Upload />
+			}
+			<SignIn user={user} />
+		</nav>
+	);
+}
